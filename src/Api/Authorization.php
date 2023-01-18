549ab849eed15dcfcb01a9f5f14cc7e2cc393823
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2023-01-18 23:16:46 +0100

    API Authorization

--- src/Api/Authorization.php
+++ src/Api/Authorization.php
@@ -6,7 +6,13 @@ use Krzysztofzylka\MicroFramework\Extension\Account;
 
 class Authorization {
 
-    public function basic(string $username, string $password) {
+    /**
+     * Basic auth
+     * @param string $username
+     * @param string $password
+     * @return bool
+     */
+    public function basic(string $username, string $password) : bool {
         try {
             return (new Account())->login($username, $password);
         } catch (\Exception) {

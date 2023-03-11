94c6d3d2701844e010cf2596798839e24583997d
krzysztof.zylka <krzysztof.zylka@wfirma.pl>
2023-03-11 15:53:30 +0100

    #14 Optimization code

--- example/app/controller/email.php
+++ example/app/controller/email.php
@@ -13,8 +13,14 @@ class email extends Controller
     {
         try {
             $email = new \Krzysztofzylka\MicroFramework\Extension\Email\Email();
+
             /** @var SendEmail $newEmail */
             $newEmail = $email->newEmail();
+
+            if (!$newEmail) {
+                throw new Exception('EMail extension is disabled');
+            }
+
             $newEmail->addAddress('address@email', 'email');
 
             if ($newEmail->send('test', 'testmail')) {
@@ -23,7 +29,7 @@ class email extends Controller
                 echo 'fail';
             }
         } catch (Exception $exception) {
-            var_dump($exception);
+            echo $exception->getMessage();
         }
     }
 
--- src/AdminPanel/controller/log.php
+++ src/AdminPanel/controller/log.php
@@ -3,6 +3,7 @@
 namespace Krzysztofzylka\MicroFramework\AdminPanel\controller;
 
 use Krzysztofzylka\MicroFramework\Controller;
+use Krzysztofzylka\MicroFramework\Extension\Table\Extra\Cell;
 
 class log extends Controller
 {

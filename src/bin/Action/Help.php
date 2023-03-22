bf69ca4c9614117eb74cd2638cec32e9a73a8e2b
krzysztof.zylka <krzysztof.zylka@wfirma.pl>
2023-03-22 05:03:54 +0100

    #25 Console

--- src/bin/Action/Help.php
+++ src/bin/Action/Help.php
@@ -18,6 +18,7 @@ class Help {
         $this->print('init                                          - Initialize project');
         $this->print('update                                        - Update project');
         $this->print('database update                               - Update database');
+        $this->print('database update_info                          - Update database - info');
         $this->print('user create <login> <password> <is admin>     - Create user');
     }
 

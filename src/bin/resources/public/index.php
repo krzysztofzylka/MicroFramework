97a243f54797f8915af401a8d79228163fbab75b
krzysztof.zylka <krzysztof.zylka@wfirma.pl>
2023-03-24 06:03:33 +0100

    remove display errors

--- src/bin/resources/public/index.php
+++ src/bin/resources/public/index.php
@@ -1,9 +1,6 @@
 <?php
 ob_start();
 session_start();
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
 use config\Config;
 use krzysztofzylka\DatabaseManager\Debug;

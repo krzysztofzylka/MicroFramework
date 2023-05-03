7d0f826f63ae245fb24ea4cfded22f434bb447c0
krzysztof.zylka <krzysztof.zylka@wfirma.pl>
2023-05-03 23:10:52 +0200

    ENV

--- src/Extension/Env/EnvInstance.php
+++ src/Extension/Env/EnvInstance.php
@@ -54,7 +54,31 @@ class EnvInstance
             }
 
             $explode = explode('=', $content, 2);
-            $_ENV[strtolower($this->fileName) . '.' . $explode[0]] = $explode[1];
+            $value = $explode[1];
+
+            if (str_starts_with($value, '"') && str_ends_with($value, '"') || str_starts_with($value, "'") && str_ends_with($value, "'")) {
+                $value = (string)substr($value, 1, -1);
+            } else {
+                if (intval($value) && substr_count($value, '.') === 0) {
+                    $value = (int)$value;
+                } elseif (floatval($value) && substr_count($value, '.') === 1) {
+                    $value = (float)$value;
+                }
+
+                switch ($value) {
+                    case 'false':
+                        $value = false;
+                        break;
+                    case 'true':
+                        $value = true;
+                        break;
+                    case 'NULL':
+                        $value = null;
+                        break;
+                }
+            }
+
+            $_ENV[strtolower($this->fileName) . '.' . $explode[0]] = $value;
         }
 
         ksort($_ENV);

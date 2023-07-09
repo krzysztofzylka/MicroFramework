b55e0ea2bd2ff4ff974ad8676dc43a79c74430f2
krzysztof.zylka <krzysztof.zylka@wfirma.pl>
2023-07-09 19:31:14 +0200

    Micro-framework     - Fix database 1688329365 script

--- src/Extension/Database/Updater/1688329365.sql
+++ src/Extension/Database/Updater/1688329365.sql
@@ -1,3 +1,1 @@
-alter table `account` modify `limit_usage` int unsigned default 0 null;
-alter table `account_remind_password` modify `account_id` int unsigned null;
 alter table `database_updater` modify `name` varchar(15) not null;

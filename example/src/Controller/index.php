36b82c3250b84e9dc63995874d7f0252a555a488
krzysztof <krzysztofzylka@yahoo.com>
2023-11-05 07:16:39 +0100

    Example

--- example/src/Controller/index.php
+++ example/src/Controller/index.php
@@ -5,6 +5,7 @@ namespace src\Controller;
 use Krzysztofzylka\MicroFramework\Controller;
 use Krzysztofzylka\MicroFramework\Exception\HiddenException;
 use Krzysztofzylka\MicroFramework\Exception\NotFoundException;
+use Krzysztofzylka\MicroFramework\Extension\DebugBar\DebugBar;
 
 class index extends Controller
 {
@@ -15,10 +16,12 @@ class index extends Controller
      */
     public function index(): void
     {
+        DebugBar::timeStart('controller');
         $this->loadModel('test');
         $this->set('variable', 'test');
-        $this->Test->find();
+        DebugBar::addMessage($this->Test->find(), 'Find');
         $this->loadView();
+        DebugBar::timeStop('controller');
     }
 
 }

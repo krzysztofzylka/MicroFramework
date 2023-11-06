a94ff272e72fcd473fa72f8aba867ca393018c7c
krzysztof <krzysztofzylka@yahoo.com>
2023-11-06 06:49:23 +0100

    vue view

--- src/Controllers/public_files.php
+++ src/Controllers/public_files.php
@@ -35,4 +35,27 @@ class public_files extends Controller
         die(readfile($path));
     }
 
+    /**
+     * Download vue file from view
+     * @param string $controller
+     * @param string $method
+     * @return void
+     * @throws NotFoundException
+     */
+    public function vue(string $controller, string $method): void
+    {
+        $path = Kernel::getPath('view') . '/' . htmlspecialchars($controller) . '/' . htmlspecialchars($method) . '.vue';
+
+        if (!file_exists($path)) {
+            throw new NotFoundException();
+        }
+
+        $fileName = basename($path);
+
+        header("Content-length: " . filesize($path));
+        header('Content-Disposition: inline; filename="' . $fileName . '"');
+        header('Content-type: text/javascript');
+        die(readfile($path));
+    }
+
 }
